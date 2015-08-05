85b634fbc5a42fe616607e501b7d8b75da659a12
JayFenchel <hannes.heinemann@web.de>
2015-08-05 10:37:04 +0200

    Update for Originaldateien

--- mpcinc_orig/mpcincfgmdynmem.c
+++ mpcinc_orig/mpcincfgmdynmem.c
@@ -22,24 +22,6 @@ struct mpcinc_fgm *mpcinc_fgm_allocate_solver(void)
     fgm->nu = (real_t *)malloc(sizeof(real_t));
     if (NULL == fgm->nu) {return NULL;}
 
-    fgm->u_ini = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
-    if (NULL == fgm->u_ini) {return NULL;}
-    fgm->u_opt = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
-    if (NULL == fgm->u_opt) {return NULL;}
-
-    fgm->tmp1_optvar_seqlen = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
-    if (NULL == fgm->tmp1_optvar_seqlen) {return NULL;}
-    fgm->tmp2_optvar_seqlen = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
-    if (NULL == fgm->tmp2_optvar_seqlen) {return NULL;}
-    fgm->tmp3_optvar_seqlen = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
-    if (NULL == fgm->tmp3_optvar_seqlen) {return NULL;}
-    fgm->tmp4_optvar_seqlen = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
-    if (NULL == fgm->tmp4_optvar_seqlen) {return NULL;}
-    fgm->tmp5_optvar_seqlen = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
-    if (NULL == fgm->tmp5_optvar_seqlen) {return NULL;}
-    fgm->tmp6_optvar_seqlen = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
-    if (NULL == fgm->tmp6_optvar_seqlen) {return NULL;}
-
     return fgm;
 }
 
@@ -58,10 +40,27 @@ mpcinc_dynmem_error_t mpcinc_fgm_setup_solver(struct mpcinc_fgm *fgm, struct mpc
     fgm->u_lb = prb->u_lb->data;
     fgm->u_ub = prb->u_ub->data;
     fgm->optvar_seqlen = prb->u_lb->rows;
-    /* fgm->optvar_veclen = ; FIXME this is needed for warmstart */
     fgm->sizeof_optvar_seqlen = sizeof(real_t) * prb->u_lb->rows;
 
     fgm->j_in = &(fgm->conf->in_iter);
+
+    fgm->u_ini = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
+    if (NULL == fgm->u_ini) {return MPCINC_DYNMEM_FAIL;}
+    fgm->u_opt = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
+    if (NULL == fgm->u_opt) {return MPCINC_DYNMEM_FAIL;}
+
+    fgm->tmp1_optvar_seqlen = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
+    if (NULL == fgm->tmp1_optvar_seqlen) {return MPCINC_DYNMEM_FAIL;}
+    fgm->tmp2_optvar_seqlen = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
+    if (NULL == fgm->tmp2_optvar_seqlen) {return MPCINC_DYNMEM_FAIL;}
+    fgm->tmp3_optvar_seqlen = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
+    if (NULL == fgm->tmp3_optvar_seqlen) {return MPCINC_DYNMEM_FAIL;}
+    fgm->tmp4_optvar_seqlen = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
+    if (NULL == fgm->tmp4_optvar_seqlen) {return MPCINC_DYNMEM_FAIL;}
+    fgm->tmp5_optvar_seqlen = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
+    if (NULL == fgm->tmp5_optvar_seqlen) {return MPCINC_DYNMEM_FAIL;}
+    fgm->tmp6_optvar_seqlen = (real_t *)malloc(fgm->sizeof_optvar_seqlen);
+    if (NULL == fgm->tmp6_optvar_seqlen) {return MPCINC_DYNMEM_FAIL;}
 
     return MPCINC_DYNMEM_OK;
 }
