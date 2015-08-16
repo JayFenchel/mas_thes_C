e99a4de31f02c0f5c9263975b6304e39febfb910
JayFenchel <hannes.heinemann@web.de>
2015-08-16 16:42:04 +0200

    setBlock und form_Y11 (letzteres im Progress)

--- hhmpcalg.c
+++ hhmpcalg.c
@@ -1,9 +1,45 @@
 #include "include/hhmpcalg.h"
 
 
-void form_Y(void)
+void form_Y(real_t Y[],
+            const real_t Phi[],
+            const uint32_t T,
+            const real_t A[], const uint32_t n,
+            const real_t B[], const uint32_t m)
 {
     
+}
+
+real_t *form_Y11(const real_t B[], const uint32_t n, const uint32_t m,
+                 const real_t R0_I[],
+                 const real_t Q1[])
+{
+    real_t BT[n*m]; /*TODO Speicher allozieren?*/
+    real_t hilf1[m]; /*TODO Speicher allozieren?*/
+    real_t hilf2[n*m]; /*TODO Speicher allozieren?*/
+    real_t sol[n*n];
+    uint32_t i;
+    mpcinc_mtx_transpose(BT, B, n, m);
+    for (i = 0; i < n; i++)
+    {
+        fwd_subst(hilf1, R0_I, m, BT+i*m);
+        bwd_subst(hilf2+i*m, R0_I, m, hilf1);
+    }
+    mpcinc_mtx_multiply_mtx_mtx();
+    mpcinc_mtx_add();
+}
+
+void setBlock(real_t mtx[], const uint32_t dim,
+              const real_t blk[], const uint32_t s,
+              const uint32_t fst)
+{
+    uint32_t i, j; /* loop counters */
+    
+    for (i = 0; i < s; i++){
+        for (j = 0; j < s; j++){
+            mtx[(i+fst)*dim+j+fst] = blk[i*s+j];
+        }
+    }
 }
 
 void getBlock(real_t blk[],
--- include/hhmpcalg.h
+++ include/hhmpcalg.h
@@ -2,12 +2,27 @@
 #define HHMPCALG_H
 
 #include "arithmetic.h"
+#include "hhmpcmath.h"
+#include "mpcincmtxops.h"
+
+
+void form_Y(real_t mtxY[],
+            const real_t mtxPhi[],
+            const uint32_t horizon,
+            const real_t mtxA[], const uint32_t dimA,
+            const real_t mtxB[], const uint32_t colsB);
+
+real_t *form_Y11(const real_t B[], const uint32_t rowsB, const uint32_t colsB,
+                 const real_t R0[],
+                 const real_t Q1[]);
+
+void setBlock(real_t mtx[], const uint32_t dimension, 
+              const real_t block[], const uint32_t size,
+              const uint32_t first);
 
 void getBlock(real_t block[], 
               const real_t mtx[], const uint32_t dimension,
               const uint32_t first,const uint32_t size);
-
-void form_Y(void);
 
 
 #endif
