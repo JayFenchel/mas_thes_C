b76d279c6b5c424b094b3c37e32dc32c097df14e
JayFenchel <hannes.heinemann@web.de>
2015-08-16 14:03:13 +0200

    Funktion um einzelne Blöcke zu kriegen

--- hhmpcalg.c
+++ hhmpcalg.c
@@ -5,3 +5,16 @@ void form_Y(void)
 {
     
 }
+
+void getBlock(real_t blk[],
+              const real_t mtx[], const uint32_t dim,
+              const uint32_t fst, const uint32_t s)
+{
+    uint32_t i, j; /* loop counters */
+    
+    for (i = 0; i < s; i++){
+        for (j = 0; j < s; j++){
+            blk[i*s+j] = mtx[(i+fst)*dim+j+fst];
+        }
+    }
+}
--- include/hhmpcalg.h
+++ include/hhmpcalg.h
@@ -1,6 +1,11 @@
 #ifndef HHMPCALG_H
 #define HHMPCALG_H
 
+#include "arithmetic.h"
+
+void getBlock(real_t block[], 
+              const real_t mtx[], const uint32_t dimension,
+              const uint32_t first,const uint32_t size);
 
 void form_Y(void);
 
