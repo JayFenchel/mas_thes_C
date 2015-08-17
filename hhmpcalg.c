223a5685a27aef318edf9042b300e0504b13b903
JayFenchel <hannes.heinemann@web.de>
2015-08-17 11:44:30 +0200

    form_Y11 inklusive Test

--- hhmpcalg.c
+++ hhmpcalg.c
@@ -10,23 +10,20 @@ void form_Y(real_t Y[],
     
 }
 
-real_t *form_Y11(const real_t B[], const uint32_t n, const uint32_t m,
-                 const real_t R0_I[],
-                 const real_t Q1[])
+void form_Y11(real_t sol[],
+              real_t B[], const uint32_t n, const uint32_t m,
+              const real_t R0_I[],
+              const real_t Q1[])
 {
-    real_t BT[n*m]; /*TODO Speicher allozieren?*/
-    real_t hilf1[m]; /*TODO Speicher allozieren?*/
-    real_t hilf2[n*m]; /*TODO Speicher allozieren?*/
-    real_t sol[n*n];
-    uint32_t i;
+    real_t BT[m*n]; /*TODO Speicher allozieren?*/
+    real_t hilf1[m*n]; /*TODO Speicher allozieren?*/
+    real_t hilf2[m*n]; /*TODO Speicher allozieren?*/
+    real_t hilf3[n*n]; /*TODO Speicher allozieren?*/
     mpcinc_mtx_transpose(BT, B, n, m);
-    for (i = 0; i < n; i++)
-    {
-        fwd_subst(hilf1, R0_I, m, BT+i*m);
-        bwd_subst(hilf2+i*m, R0_I, m, hilf1);
-    }
-    mpcinc_mtx_multiply_mtx_mtx();
-    mpcinc_mtx_add();
+    fwd_subst(hilf1, R0_I, m, BT, n);
+    bwd_subst(hilf2, R0_I, m, hilf1, n);
+    mpcinc_mtx_multiply_mtx_mtx(hilf3, B, hilf2, n, m, n);
+    mpcinc_mtx_add(sol, hilf3, Q1, n, n);
 }
 
 void setBlock(real_t mtx[], const uint32_t dim,
--- include/hhmpcalg.h
+++ include/hhmpcalg.h
@@ -12,9 +12,10 @@ void form_Y(real_t mtxY[],
             const real_t mtxA[], const uint32_t dimA,
             const real_t mtxB[], const uint32_t colsB);
 
-real_t *form_Y11(const real_t B[], const uint32_t rowsB, const uint32_t colsB,
-                 const real_t R0[],
-                 const real_t Q1[]);
+void form_Y11(real_t solution[],
+              real_t B[], const uint32_t rowsB, const uint32_t colsB,
+              const real_t R0_I[],
+              const real_t Q1[]);
 
 void setBlock(real_t mtx[], const uint32_t dimension, 
               const real_t block[], const uint32_t size,
