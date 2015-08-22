9a82c0c15c85157344ede8a5d870423b1f127e02
JayFenchel <hannes.heinemann@web.de>
2015-08-22 13:13:30 +0200

    weitere Variablen zu ipm hinzugefügt

--- include/hhmpcipm.h
+++ include/hhmpcipm.h
@@ -12,13 +12,20 @@ struct hhmpc_ipm_conf {
 
 struct hhmpc_ipm {
     struct hhmpc_ipm_conf *conf;  /* Algorithm configuration data. */
+    real_t *z_opt;  /* Solution to the optimal control problem. */
+    real_t *z_ini;  /* Initial guess for the optimal control sequence. */
     real_t *q;
     real_t *r;
     
+    uint32_t *j_in;
+    
     real_t *kappa;  /* Barrier parameter. */
+    uint32_t horizon; /* Prediction horizon. */
     uint32_t optvar_veclen;  /* The length of each vector in the optimation variable sequence. */
     uint32_t optvar_seqlen;  /* The full length of optimization variable sequence. */
     uint32_t sizeof_optvar_seqlen;  /* Number of bytes in the optimization variable sequence. */
+    
+    real_t *tmp1_optvar_seqlen;  /* Temporary variable of length optvar_seqlen. */
 };
 
 
