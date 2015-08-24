6f436ad679d2ba703547aa265027035dbf1b0428
JayFenchel <hannes.heinemann@web.de>
2015-08-24 16:13:20 +0200

    berechnung von pmetric in form_problem

--- hhmpcsocp.c
+++ hhmpcsocp.c
@@ -1,4 +1,5 @@
-#include "hhmpcsocp.h"
+#include "include/hhmpcsocp.h"
+#include "include/mpcincmtxops.h"
 
 
 static void hhmpc_copy_data(struct hhmpc_term *dest, struct hhmpc_term *src);
@@ -6,6 +7,19 @@ static void hhmpc_copy_data(struct hhmpc_term *dest, struct hhmpc_term *src);
 void hhmpc_socp_form_problem(struct hhmpc_socp *socp)
 {
     /* Needed if parametric Matrices are used*/
+    int i, j;
+    struct hhmpc_pmetric *pm;
+    
+    for (i = 0;  i < HHMPC_PMETRIC_NUM; i++) {
+        pm = socp->pmetric[i];
+        hhmpc_copy_data(pm->val, pm->fac0);
+        for (j = 0; j < pm->fac_num[0]; j++){
+            mpcinc_mtx_mul_add(pm->val->data, pm->aux->data,
+            pm->fac[j]->data, pm->par[j]->data,
+            pm->fac[j]->rows, pm->fac[j]->cols);
+        }
+    }
+    
 }
 
 void hhmpc_copy_data(struct hhmpc_term *dest, struct hhmpc_term *src)  {
