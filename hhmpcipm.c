3e966727eb9a5df464b39f9c78ccb62eb99d0478
JayFenchel <hannes.heinemann@web.de>
2015-08-25 15:35:29 +0200

    residual berechnen ohne 2*H*z Term

--- hhmpcipm.c
+++ hhmpcipm.c
@@ -20,12 +20,13 @@ void hhmpc_ipm_solve_problem(const struct hhmpc_ipm *ipm)
     hhmpc_ipm_check_valid(ipm);
     /*Take initial value*/
     memcpy(ipm->z_opt, ipm->z_ini, ipm->sizeof_optvar_seqlen);
+    memcpy(ipm->v_opt, ipm->v_ini, ipm->sizeof_dual_seqlen);
     
     /*Improve z for a fixed number of steps j_in*/
     for (j = 0; j < *(ipm->j_in); j++) {
         /* Calculate the residual */
-        form_d(ipm->d, ipm->P, ipm->h, ipm->z_opt, 36, ipm->optvar_seqlen);
         residual(ipm);
+        print_mtx(ipm->r_d, ipm->optvar_seqlen, 1);
         print_mtx(ipm->r_p, ipm->dual_seqlen, 1);
         /* Solve system of linear equations to obtain the step direction */
         /* Find best step size (0...1] */
@@ -41,10 +42,21 @@ void hhmpc_ipm_check_valid(const struct hhmpc_ipm *ipm)
 
 void residual(const struct hhmpc_ipm *ipm)
 {
-    /*print_mtx(ipm->z_ini, ipm->optvar_seqlen, 1);*/
-    real_t *help = ipm->tmp2_dual_seqlen;
+    real_t one[] = {1.};
+    real_t *help = ipm->tmp1_optvar_seqlen;
+    real_t *help2 = ipm->tmp2_dual_seqlen;
+    
+    form_d(ipm->d, ipm->P, ipm->h, ipm->z_opt, 36, ipm->optvar_seqlen);
+    
+    mpcinc_mtx_multiply_mtx_vec(help, ipm->P_T, ipm->d, ipm->optvar_seqlen, 36);
+    mpcinc_mtx_scale(ipm->r_d, help, 0.015, ipm->optvar_seqlen, 1);
+    mpcinc_mtx_mul_add(ipm->r_d, help, ipm->C_T, ipm->v_opt,
+                       ipm->optvar_seqlen, ipm->dual_seqlen);
+    mpcinc_mtx_mul_add(ipm->r_d, help, ipm->g, one,
+                       ipm->optvar_seqlen, 1);
+    
     mpcinc_mtx_scale(ipm->r_p, ipm->b, -1, ipm->dual_seqlen, 1);
-    mpcinc_mtx_mul_add(ipm->r_p, help, ipm->C, ipm->z_opt,
+    mpcinc_mtx_mul_add(ipm->r_p, help2, ipm->C, ipm->z_opt,
                        ipm->dual_seqlen, ipm->optvar_seqlen);
     
     print_mtx(ipm->P, 36, 15);
