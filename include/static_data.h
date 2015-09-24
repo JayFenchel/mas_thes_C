afda7166550cfa68e12867c6526b009f7a36525a
JayFenchel <hannes.heinemann@web.de>
2015-09-24 10:38:42 +0200

    Data for QP of PCE without chance constraints

--- include/static_data.h
+++ include/static_data.h
@@ -1,12 +1,14 @@
 #ifndef STATIC_DATA_H
 #define STATIC_DATA_H
 
+#define HHMPC_QPTEST  /* Define desired example here. */
 
 #include "hhmpcsocp.h"
 #include "hhmpcipm.h"
 #include "mpcincmtxops.h"
 #include "hhmpcusefull.h"
 
+#ifdef HHMPC_QPTEST
 
 #define HHMPC_HORIZON 5
 #define HHMPC_SV 30
@@ -14,11 +16,13 @@
 #define HHMPC_OV 31
 #define HHMPC_OS 155
 #define HHMPC_DS 150
-#define HHMPC_NB_LCONSTR 34
+#define HHMPC_NB_LCONSTR 22
 #define HHMPC_NB_QC 0
-#define HHMPC_NB_SOCC 5
+#define HHMPC_NB_SOCC 0
 #define HHMPC_NB_IEQ (HHMPC_NB_LCONSTR+HHMPC_NB_QC+HHMPC_NB_SOCC)
-#define HHMPC_NB_SOFT 34
+#define HHMPC_NB_SOFT 0
+
+#endif
 
 extern void form_ipm(struct hhmpc_ipm *ipm, struct hhmpc_socp_prb *prb);
 
